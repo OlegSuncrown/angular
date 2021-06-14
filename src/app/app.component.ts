dfd6678f9aa7e636da80d3a6987745fb23eeb124
Oleg O <oleg_biblyi@epam.com>
2021-06-14 20:52:52 +0300

    set up ngrx and router store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectUrl } from './core/@ngrx/router/router-selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'notes';
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.select(selectUrl).subscribe((val) => {
+      console.log(val)
+    })
+  }
 }
--- src/app/core/@ngrx/root-store.module.ts
+++ src/app/core/@ngrx/root-store.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { environment } from './../../../environments/environment';
+
+// Store
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+
+// Router store
+import { CustomSerializer, routerReducers } from './router';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-  ]
+    StoreModule.forRoot(routerReducers, {
+      // All checks will automatically be disabled in production builds
+      runtimeChecks: {
+        strictStateImmutability: true, // default value is true
+        strictActionImmutability: true, // default value is true
+      },
+    }),
+    // Connects RouterModule with StoreModule, uses MinimalRouterStateSerializer by default
+    StoreRouterConnectingModule.forRoot({
+      serializer: CustomSerializer,
+    }),
+
+    EffectsModule.forRoot([]),
+    // Instrumentation must be imported after importing StoreModule (config is optional)
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+  ],
 })
-export class RootStoreModule { }
+export class RootStoreModule {}
